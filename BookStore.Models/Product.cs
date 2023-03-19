b3717c2367615886f14d1f8eac877702dc743768
Konstantinos Ntountoumis <kon7865@hotmail.com>
2023-03-19 20:05:13 +0200

    Bookstore

---
+++ BookStore.Models/Product.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		public string Description { get; set; }
+		[Required]
+		public string ISBN { get; set; }
+		[Required]
+		public string Author { get; set; }
+		[Required]
+		[Range(1, 10000)]
+        [Display(Name = "List Price")]
+        public double ListPrice { get; set; }
+		[Required]
+		[Range(1, 10000)]
+        [Display(Name = "Price for 1-50")]
+        public double Price { get; set; }
+
+		[Required]
+		[Range(1, 10000)]
+        [Display(Name = "Price 51-100")]
+        public double Price50 { get; set; }
+
+		[Required]
+		[Range(1, 10000)]
+        [Display(Name = "Price 100+")]
+        public double Price100 { get; set; }
+		[ValidateNever]
+		public string ImageUrl { get; set; }
+
+		[Required]
+		public int CategoryId { get; set; }
+		[ForeignKey("CategoryId")]
+		[ValidateNever]
+        [Display(Name = "Category")]
+        public Category Category { get; set; }
+
+		[Required]
+		public int CoverTypeId { get; set; }
+		[ValidateNever]
+        [Display(Name = "Cover Type")]
+        public CoverType CoverType { get; set; }
+	}
+}
